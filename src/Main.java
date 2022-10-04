c441c4a1ceaf2cef2d69a864caa7a01bb37f5509
yura178500 <yura178500@yandex.ru>
2022-10-04 16:35:00 +0300

    Задание 4-5

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(//Задание 4);
+        var friend = 19;
+        System.out.print(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        System.out.println(//Задание 5);
+        var frog = 3.5;
+        System.out.printin(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+}
